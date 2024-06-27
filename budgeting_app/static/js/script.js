e57185320e5bca966b238241fd07421028e5f934
BeatHerSppark <peter@halkyon.com>
2024-06-27 19:25:13 +0200

    finished sidebar design

--- budgeting_app/static/js/script.js
+++ budgeting_app/static/js/script.js
@@ -1,5 +1,13 @@
 const burgerMenu = document.getElementById("toggle-btn");
+const sidebarLogo = document.querySelector(".sidebar-logo")
+
+window.onload = () => {
+    document.getElementById("sidebar").classList.toggle("expand");
+}
 
 burgerMenu.addEventListener("click", () => {
+    document.getElementById("sidebar").classList.toggle("expand");
+})
+sidebarLogo.addEventListener("click", () => {
     document.getElementById("sidebar").classList.toggle("expand");
 })
