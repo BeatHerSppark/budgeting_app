204b94da02a87158c4f64915a2b5d794f08f291e
BeatHerSppark <peter@halkyon.com>
2024-06-29 18:38:08 +0200

    test model ubuntu

--- budgeting_app/app/models.py
+++ budgeting_app/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ('expense', 'Expense'),
+        ('earning', 'Earning'),
+    ]
+
+    sum_of_money = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPE_CHOICES)
+    date_of_transaction = models.DateTimeField()
+    comment = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.get_transaction_type_display()} of {self.sum_of_money} on {self.date_of_transaction}"
