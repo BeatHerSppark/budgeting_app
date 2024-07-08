93956fac956a01b9974be51042b62a3c0ddaae33
BeatHerSppark <peter@halkyon.com>
2024-07-08 13:20:11 +0200

    added category and icons model

--- budgeting_app/app/admin.py
+++ budgeting_app/app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Transaction
+from .models import Transaction, Category, Icon
 
 # Register your models here.
 admin.site.register(Transaction)
+admin.site.register(Category)
+admin.site.register(Icon)
--- budgeting_app/app/models.py
+++ budgeting_app/app/models.py
@@ -3,6 +3,21 @@ from users.models import Profile
 from django.utils.timezone import now
 
 # Create your models here.
+class Category(models.Model):
+    CHOICES = [
+        ('expense', 'Expense'),
+        ('income', 'Income')
+    ]
+    category_type = models.CharField(max_length=7, choices=CHOICES)
+    default_category = models.BooleanField(default=False) # Premade categories have value True, user-made categories have value False
+    title = models.CharField(max_length=50)
+    icon_tag = models.CharField(max_length=50)
+    profile = models.ForeignKey(to=Profile, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.title
+
+
 class Transaction(models.Model):
     CHOICES = [
         ('expense', 'Expense'),
@@ -12,4 +27,10 @@ class Transaction(models.Model):
     amount = models.FloatField()
     date = models.DateField(default=now)
     profile = models.ForeignKey(to=Profile, on_delete=models.CASCADE, default=None)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, default=None)
     comment = models.TextField(max_length=200, blank=True, null=True)
+
+
+class Icon(models.Model):
+    icon_tag = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
