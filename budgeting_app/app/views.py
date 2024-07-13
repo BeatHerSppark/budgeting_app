ceaa9a7aa6b46c21c3177660f74d3ba1d749de8e
BeatHerSppark <peter@halkyon.com>
2024-07-13 02:38:45 +0200

    adding transactions expenses and incomes

--- budgeting_app/app/views.py
+++ budgeting_app/app/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
-from .models import Transaction
+from .models import Transaction, Category
 from . import forms
 
 # Create your views here.
 @login_required(login_url="/users/login/")
 def dashboard_view(request):
     form = forms.AddTransaction()
+    expenseCategories = Category.objects.filter(category_type="expense")
+    print(expenseCategories)
+    incomeCategories = Category.objects.filter(category_type="Income")
     #current_day = datetime.now().strftime('%Y-%m-%d')
     #current_week = datetime.now().strftime('%Y-W%V')
     recent_transactions = Transaction.objects.filter(profile=request.user.profile).order_by("date")[:10]
-    print(recent_transactions)
-    return render(request, "app/dashboard.html", { "user": request.user, "recent_transactions": recent_transactions, "form": form },)
+    return render(request, "app/dashboard.html", {
+        "user": request.user,
+        "recent_transactions": recent_transactions,
+        "form": form,
+        "expenseCategories": expenseCategories,
+        "incomeCategories": incomeCategories,
+    },)
 
 @login_required(login_url="/users/login/")
 def transactions_view(request):
